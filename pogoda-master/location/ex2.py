a1bdd05fd249c4c1d4e2c8be1015f3c367cafc5c
Настасова Варя <78798769+varezkayo@users.noreply.github.com>
2021-12-17 20:58:58 +0300

    Update ex2.py

--- pogoda-master/location/ex2.py
+++ pogoda-master/location/ex2.py
@@ -2,7 +2,7 @@ YOUR_API_KEY = 'API_KEY'
 google_places = GooglePlaces(YOUR_API_KEY)
 
 query_result = google_places.nearby_search(
-        lat_lng={'lat': 52.0737017, 'lng': 5.0944107999999915}, 
+        lat_lng={'lat': 52.0737017, 'lng': 5.0944107999999915}, #пользователь должен сам ввести lat и lng 
         radius=100,
         types=[types.TYPE_RESTAURANT] or [types.TYPE_CAFE])
 
