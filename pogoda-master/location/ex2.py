a193c954cc661b2549c4048dd9eee635095ffd55
Настасова Варя <78798769+varezkayo@users.noreply.github.com>
2021-12-17 20:45:49 +0300

    Create ex2.py

---
+++ pogoda-master/location/ex2.py
@@ -0,0 +1,11 @@
+YOUR_API_KEY = 'API_KEY'
+google_places = GooglePlaces(YOUR_API_KEY)
+
+query_result = google_places.nearby_search(
+        lat_lng={'lat': 52.0737017, 'lng': 5.0944107999999915}, 
+        radius=100,
+        types=[types.TYPE_RESTAURANT] or [types.TYPE_CAFE])
+
+for place in query_result.places:
+     place.get_details()
+     print '%s %s %s' % (place.name, place.geo_location, place.types)
