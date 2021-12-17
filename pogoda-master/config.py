819f0d09db347f9601dd2bba8675a76e4c7a1ee7
Настасова Варя <78798769+varezkayo@users.noreply.github.com>
2021-12-17 22:32:32 +0300

    Update config.py

--- pogoda-master/config.py
+++ pogoda-master/config.py
@@ -1,3 +1,2 @@
 open_weather_token = "79c314e3a16bb18c814409f2c1b91d68"
-tg_bot_token = "5069121340:AAEFDLJLYe2HmXmBSTMhmPaj_aTI-rB0k7c
-"
+tg_bot_token = "5069121340:AAEFDLJLYe2HmXmBSTMhmPaj_aTI-rB0k7c"
